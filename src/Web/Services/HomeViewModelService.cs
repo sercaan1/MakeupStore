99ec8e6f56e33402cb5f167d0190c55541d9058e
sercaan1 <sercankarakuyu@gmail.com>
2022-06-15 14:45:58 +0300

    products filter

--- src/Web/Services/HomeViewModelService.cs
+++ src/Web/Services/HomeViewModelService.cs
@@ -1,5 +1,6 @@
 ﻿using ApplicationCore.Entities;
 using ApplicationCore.Interfaces;
+using ApplicationCore.Specifications;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using System;
 using System.Collections.Generic;
@@ -22,9 +23,10 @@ namespace Web.Services
             _brandRepo = brandRepo;
             _categoryRepo = categoryRepo;
         }
-        public async Task<HomeViewModel> GetHomeViewModelAsync()
+        public async Task<HomeViewModel> GetHomeViewModelAsync(int? brandId, int? categoryId)
         {
-            var products = await _productRepo.GetAllAsync();
+            var specProducts = new ProductsFilterSpecification(brandId, categoryId);
+            var products = await _productRepo.GetAllAsync(specProducts);
 
             var viewModel = new HomeViewModel()
             {
@@ -36,7 +38,9 @@ namespace Web.Services
                     Price = x.Price
                 }).ToList(),
                 Brands = await GetBrandsAsync(),
-                Categories = await GetCategoriesAsync()
+                Categories = await GetCategoriesAsync(),
+                BrandId = brandId,
+                CategoryId = categoryId
             };
 
             return viewModel;
