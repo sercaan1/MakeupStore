16fda2b8b17c5847ecd25b1f78cf684710b37f4b
sercaan1 <sercankarakuyu@gmail.com>
2022-06-24 17:27:55 +0300

    Unit tests

---
+++ src/ApplicationCore/Exceptions/NonpositiveQuantityException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Exceptions
+{
+    public class NonpositiveQuantityException : Exception
+    {
+        public NonpositiveQuantityException() : base("At least one item")
+        {
+        }
+
+        public NonpositiveQuantityException(string message) : base(message)
+        {
+        }
+
+        public NonpositiveQuantityException(string message, Exception inner) : base(message, inner)
+        {
+        }
+    }
+}
